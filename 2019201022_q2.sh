34b9071fe8701a41508fa236c8475214e0ed9ef5
divy-kala <divy.kala@students.iiit.ac.in>
2019-09-16 04:41:57 +0530

    script 2, file opener fixed

--- 2019201022_q2.sh
+++ 2019201022_q2.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
-fname=$1
-mtype=`file --mime-type $fname`
-mytype=${mtype#*: }
-echo $mytype
-line=`grep "$mytype" /usr/share/applications/defaults.list`
-echo $line
+fname="$1"
+mytype=`file --mime-type "$fname"`
+#echo reading $fname
+mytype=${mytype#*: }
+#echo type is $mytype
+line=`grep -E -m 1 "$mytype" /usr/share/applications/defaults.list`
+if [ "$?" != "0" ] ; then
+	echo "couldnt find file type in defaults.list. exiting"
+	exit 1
+fi
+#echo line is $line
 binary=${line#*=}
-echo $binary
 binary=${binary%%;*}
-echo $binary
-exec=`grep -o "Exec=.* " "/usr/share/applications/$binary"`
+#echo binary is $binary
+exec=`grep -o -m 1 "Exec=.* " "/usr/share/applications/$binary"`
+if [ "$?" != "0" ] ; then
+	echo "couldnt open $binary"
+	exit 1
+fi
+#echo line is $line
 exec=${exec#*=}
-$exec $file
+#echo exec is $exec
+#echo file is $fname
+$exec "$fname"
